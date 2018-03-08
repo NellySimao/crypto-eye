07460c722410e15082e50d5f12ab98ab5384a6ad
Nelly Simao <Nelson@Nelsons-MacBook.local>
2018-03-07 18:06:37 -0800

    added coins

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 var NumberFormat = require('react-number-format');
+// let NumeralInput = require('react-numeral-input');
+
 
 
 
@@ -19,10 +21,9 @@ class App extends Component {
 
 
   componentWillMount() {
-    axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,KNC&tsyms=USD,EUR')
+    axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,LTC,ADA,NEO,XLM,XMR,EOS,IOT,DASH,XEM,TRX,ETC,VEN,GJC,USDT,QTUM,BTG,LSK,OMG,ICX,ZEC,DGD,UCASH,BNB,STEEM,BCN,PPT,XVG,STRAT,WAVES,SNT,WTC,RHOC,SC,MKR,DOGE,BTS,AE,DCR,REP,ZRX,KMD,BTM,ETN,VERI,ARDR,ARK,ACT,DRGN,GNT,HSR,SYS,BAT,CNX,ETHOS,PIVX,ZIL,DGB&tsyms=USD,EUR')
       .then(results => {
         const cryptos = results.data.RAW;
-        const cryptoEur = results.data;
         console.log(cryptos);
         this.setState({cryptos: cryptos});
       })
@@ -31,16 +32,43 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" onClick={myFunction}>
-        {Object.keys(this.state.cryptos).map((key) => (
-          <div id="crypto-container">
-            <span className="crypto">{key}</span>
-            <span id="dollar"><NumberFormat value={this.state.cryptos[key].USD.PRICE} displayType={'text'} decimalprecision={2} thousandSeparator={true} prefix={'$'} />
-</span>
-            <span id="euro"><NumberFormat value={this.state.cryptos[key].EUR.PRICE} displayType={'text'} decimalprecision={2} thousandSeparator={true} prefix={'€'} />
-</span>
+      <div className="App">
+        <div className="header">
+          <h1 className="header--title">Crypto Eye</h1>
+        </div>
+
+          <div className="crypto-container">
+            <div className="table-container">
+              <table className="crypto-table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Coin</th>
+                    <th></th>
+                    <th className="text-right">Price</th>
+                    {/* <th className="text-right">1 Hour</th>
+                    <th className="text-right">1 Day</th>
+                    <th className="text-right">1 Week</th> */}
+                    <th className="text-right">Market Cap</th>
+                    {/* <th className="text-right">24h Volume</th> */}
+                  </tr>
+                </thead>
+                <tbody>
+                  {Object.keys(this.state.cryptos).map((key) => (
+                  <tr className="coin-row">
+                    <th></th>
+                    <td className="crypto">{key}</td>
+                    <th></th>
+
+                    <td className="dollar"><NumberFormat value={this.state.cryptos[key].USD.PRICE} displayType={'text'} decimalprecision={2} thousandSeparator={true} prefix={'$'} /></td>
+                    <td className="marketcap"><NumberFormat value={this.state.cryptos[key].USD.MKTCAP} displayType={'text'} decimalprecision={2} thousandSeparator={true} prefix={'$'} /></td>
+                  </tr>
+                  
+                ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        ))}
       </div>
     );
   }
@@ -49,11 +77,16 @@ class App extends Component {
   
 export default App;
 
-function myFunction() {
-  var x = document.getElementById("euro");
-  if (x.style.display === "none") {
-      x.style.display = "block";
-  } else {
-      x.style.display = "none";
-  }
-}
+
+
+// function myFunction(e) {
+//   var x = e.target.childNodes[2];
+//   var y = e.target.childNodes[1];
+//   if (x.style.display === "block") {
+//       y.style.display = "none";
+//   } else {
+//       x.style.display = "block";
+//       y.style.display = "none";
+//   }
+// console.log(e.target.childNodes);
+// }
